48c60b8e64b0f47301300645aeaeb87a77b30b2e
timcheee <timotej.friskovec@gmail.com>
2016-11-06 20:19:08 +0100

    movement-fixed

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,63 +2,55 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+    public Transform target;
+    public int moveSpeed;
+    public int rotationSpeed;
 
-    // Use this for initialization
-    GameObject pathGO;
-    Transform targetPathNode;
-    int pathNodeIndex = 0;
-    float speed = 5f;
-    public int health = 3;
+    private Transform myTransform;
+
+
+    void Awake()
+    {
+        myTransform = transform; 
+    }
+
     void Start () {
-        pathGO = GameObject.Find("Path");
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        target = go.transform;
 	}
 
-    void GetNextPathNode()
-    {
-        if(pathNodeIndex < pathGO.transform.childCount)
-        {
-            targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
-            pathNodeIndex++;
-        }
-        else
-        {
-            targetPathNode = null;
-            ReachedGoal();
-            
-        }
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	    if(targetPathNode == null)
-        {
-            GetNextPathNode();
-            if(targetPathNode == null)
-            {
-                //enemy je na cilju
-                ReachedGoal();
-                return;
-            }
-        }
+    // Update is called once per frame
+    void Update () {
 
-        
-        Vector3 dir = targetPathNode.position - this.transform.localPosition;
-        float distThisFrame = speed * Time.deltaTime;
-        if(dir.magnitude <= distThisFrame)
-        {
-            targetPathNode = null;
-        }
-        else
-        {
-            transform.Translate(dir.normalized * distThisFrame);
-            //this.transform.rotation =  Quaternion.LookRotation(dir);
-        }
-        
-	}
-   void ReachedGoal()
-    {
-        Destroy(gameObject);
+        Debug.DrawLine(target.position, myTransform.position, Color.blue);
+
+        //Da gleda v tarčo
+        //myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        //Kreni proti tarči
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+        Attack();
+        //OnTriggerEnter();
+
     }
+    public void Attack()
+    {
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+        //Debug.Log(distance);
+        Debug.Log(myTransform.position.x);
+        if(distance < 1.5)
+        {
+           // moveSpeed = 0;
+        }
+        //ko je mob dost blizu zida za melle attack se ustavi
+        if(myTransform.position.x +1.5 > target.position.x)
+        {
+            moveSpeed = 0;
+        }
+        //TODO: mob začne napadat zid
+
+    }
+
+
 
 }
