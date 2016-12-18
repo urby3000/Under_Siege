4385d0b69e3d83a786b17ee10ebf5178fdc4479c
Boshty15 <bostjan@kostomaj.org>
2016-12-18 21:32:39 +0100

    Add files via upload

---
+++ Assets/UI/Scripts/WallHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallHealth : MonoBehaviour {
+
+    public float max_health = 100f;
+    public float health = 0f;
+    public GameObject bar;
+
+	// Use this for initialization
+	void Start () {
+        health = max_health;
+        health = PlayerPrefs.GetFloat("Current_hp");
+        decreaseHealth();
+        //InvokeRepeating("decreaseHealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void decreaseHealth()
+    {
+        health -= 2f;
+        float calc_health = health / max_health;
+        setHealthBar(calc_health);
+    }
+    void setHealthBar(float myhealth)
+    {
+        bar.transform.localScale = new Vector3(myhealth, bar.transform.localScale.y, bar.transform.localScale.z);
+    }
+}
